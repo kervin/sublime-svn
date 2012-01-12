a2f03ee9f386dcb3d2fe108b6aef3b1b727a775d
Kervin Ramen <kervin@kervin-thinkpad.site>
2012-01-12 11:49:59 +0400

    first commit

---
+++ Rabbitvcs.py
@@ -0,0 +1,69 @@
+import sublime
+import sublime_plugin
+import os
+import sys
+import subprocess
+import locale
+
+if os.name == 'nt':
+    import _winreg
+
+
+class NotFoundError(Exception):
+    pass
+
+class RabbitCommand():
+
+    def get_path(self, paths):
+        if paths:
+            return paths[0]
+        elif self.window.active_view():
+            return self.window.active_view().file_name()
+        elif self.window.folders():
+            return self.window.folders()[0]
+        else:
+            sublime.error_message(__name__ + ': No place to open terminal to')
+    
+    def commit(self, paths):
+        path = self.get_path(paths)
+        if not path:
+            return
+
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+        
+        subprocess.call("rabbitvcs commit " + path, shell=True) 
+    
+    def update(self, paths):
+        path = self.get_path(paths)
+        if not path:
+            return
+
+
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+        
+        subprocess.call("rabbitvcs update " + path, shell=True)
+
+    def browse(self, paths):
+        path = self.get_path(paths)
+        if not path:
+            return
+
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+        
+        subprocess.call("rabbitvcs browser " + path, shell=True)
+
+
+class RabbitCommitCommand(sublime_plugin.WindowCommand, RabbitCommand):
+    def run(self, paths=[], parameters=None):
+        self.commit(paths)
+
+class RabbitUpdateCommand(sublime_plugin.WindowCommand, RabbitCommand):
+    def run(self, paths=[], parameters=None):
+        self.update(paths)
+
+class RabbitBrowseCommand(sublime_plugin.WindowCommand, RabbitCommand):
+    def run(self, paths=[], parameters=None):
+        self.browse(paths)
